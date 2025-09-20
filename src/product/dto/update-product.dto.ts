ba9dcfa9a19d34ccd3ecf7a25a3adfed83f2137a
JonyGudino21 <gudinojonathan35@gmail.com>
2025-09-19 20:08:23 -0600

    feat: :sparkles: Feature crud of module of Products

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+
+  @IsString()
+  @IsOptional()
+  strength?: string;  // 500mg, 1000mg, etc.
+
+  @IsString()
+  @IsOptional()
+  format?: string;   // Tableta, capsula, etc.
+
+  @IsString()
+  @IsOptional()
+  presentation?: string; // Adulto, infantil, pediátrico
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,73 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationParamsDto } from 'src/common/dto/pagination-params.dto';
+import { ApiResponse } from 'src/common/dto/response.dto';
+import { SearchProductDto } from './dto/search-product.dto';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() createProductDto: CreateProductDto, @GetUser('id') userId: number) {
+    const res = await this.productService.create(createProductDto, userId);
+    return ApiResponse.ok(res,'Producto creado correctamente');
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  async findAll(@Query('active') active?: string, @Query() pagination?: PaginationParamsDto) {
+    console.log(active);
+    let isActive: boolean | undefined;
+    if (active === 'true') {
+      isActive = true;
+    } else if (active === 'false') {
+      isActive = false;
+    }
+    const res = await this.productService.findAll(isActive, pagination);
+    return ApiResponse.ok(res, 'Producto encontrado exitosamente');
+  }
+
+  @Post('search')
+  async search(@Body() searchDto: SearchProductDto) {
+    const { name, letters, ...pagination } = searchDto;
+    const res = await this.productService.search(name, letters, pagination);
+    return ApiResponse.ok(res, 'Producto encontrado exitosamente');
+  }
+
+  @Get('sku/:sku')
+  async findBySku(@Param('sku') sku: string) {
+    const res = await this.productService.findBySku(sku);
+    return ApiResponse.ok(res, 'Producto encontrado exitosamente');
+  }
+  
+  @Get('barcode/:barcode')
+  async findByBarcode(@Param('barcode') barcode: string) {
+    const res = await this.productService.findByBarcode(barcode);
+    return ApiResponse.ok(res, 'Producto encontrado exitosamente');
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  async findOne(@Param('id') id: number) {
+    const res = await this.productService.findOne(id);
+    return ApiResponse.ok(res, 'Producto encontrado exitosamente');
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  @UseGuards(JwtAuthGuard)
+  async update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto, @GetUser('id') userId: number) {
+    const res = await this.productService.update(id, updateProductDto, userId);
+    return ApiResponse.ok(res, 'Producto actualizado exitosamente');
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  async remove(@Param('id') id: number) {
+    const res = await this.productService.remove(id);
+    return ApiResponse.ok(res, 'Producto eliminado exitosamente');
   }
 }
