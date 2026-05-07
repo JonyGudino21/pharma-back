ca70d671bd0d106fc79aaf560fa4d73a1b6dcb91
JonyGudino21 <gudinojonathan35@gmail.com>
2026-05-06 22:46:34 -0600

    refactor: :white_check_mark: Manage the new information of a product and implements in the CRUD module

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -6,16 +6,4 @@ export class UpdateProductDto extends PartialType(CreateProductDto) {
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
-
-  @IsString()
-  @IsOptional()
-  strength?: string;  // 500mg, 1000mg, etc.
-
-  @IsString()
-  @IsOptional()
-  format?: string;   // Tableta, capsula, etc.
-
-  @IsString()
-  @IsOptional()
-  presentation?: string; // Adulto, infantil, pediátrico
 }
